275f804cd72fa77605cd8f7fe715e778af1a71ba
Nina <nina@ninas-MacBook-Pro.local>
2023-08-24 12:39:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/iotgateway/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.iotgateway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initUI()
+    }
+    private fun initUI(){
+        setContentView(R.layout.activity_main)
+        val connectBtn = findViewById<Button>(R.id.connect_btn)
+        connectBtn.setOnClickListener{
+            connectMqttServer()
+        }
+    }
+    private fun connectMqttServer(){
+
+    }
+}                   
